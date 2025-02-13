6963cdc6e22d00b586302bc3f6b915cac5dfb006
Ahamed Munsif <113419042+munsif-dev@users.noreply.github.com>
2025-02-13 21:27:43 +0530

    test commit

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -50,7 +50,7 @@ export default function Layout() {
                 </span>
               </Link>
             </div>
-            {/* <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
+            {/* <Button variant="outline" f size="icon" className="ml-auto h-8 w-8">
               <Bell className="h-4 w-4" />
               <span className="sr-only">Toggle notifications</span>
             </Button> */}
